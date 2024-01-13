01a6decb19fe7b63c292a9e431fc56e0ad329737
Taweechokk <145736999+Taweechokk@users.noreply.github.com>
2024-01-13 23:21:53 +0700

    Add files via upload

---
+++ Puzzle/Xmas tree.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j;
+    for(i=0; i>11; i++)
+    {
+        for(j=0; j<i; i++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    printf("Marry X'mas");
+
+}
